c91806cfd37f3228926eab2f26dbdfa4503523a8
Lars Westermann <lars-westermann@live.de>
2020-08-03 15:01:55 +0200

    Update screen example

--- examples/screen/src/main.rs
+++ examples/screen/src/main.rs
@@ -1,10 +1,18 @@
 extern crate ev3dev_lang_rust;
-extern crate framebuffer;
 extern crate image;
+extern crate imageproc;
 
 use image::Rgb;
 
 use ev3dev_lang_rust::Screen;
+
+fn interpolate(a: Rgb<u8>, b: Rgb<u8>, progress: f32) -> Rgb<u8> {
+    Rgb([
+        ((a.0[0] as f32) * (1.0 - progress) + (b.0[0] as f32) * progress) as u8,
+        ((a.0[1] as f32) * (1.0 - progress) + (b.0[1] as f32) * progress) as u8,
+        ((a.0[2] as f32) * (1.0 - progress) + (b.0[2] as f32) * progress) as u8,
+    ])
+}
 
 fn main() {
     let mut screen = Screen::new().unwrap();
@@ -30,6 +38,16 @@ fn main() {
             screen.image.put_pixel(x, y, Rgb([0, 0, 255]));
         }
     }
+
+    imageproc::drawing::draw_antialiased_line_segment_mut(
+        &mut screen.image,
+        (10, 50),
+        (50, 90),
+        Rgb([0, 0, 0]),
+        interpolate,
+    );
+
+    imageproc::drawing::draw_filled_circle_mut(&mut screen.image, (100, 50), 40, Rgb([0, 0, 255]));
 
     screen.update();
 }
