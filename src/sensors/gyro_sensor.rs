728d86d25139b7d3d680a494fcb13ca9657e8cfa
Massimo Gismondi <omino.gis@gmail.com>
2021-01-05 17:36:36 +0100

    missing modes and performance improvement in gyro sensor

--- src/sensors/gyro_sensor.rs
+++ src/sensors/gyro_sensor.rs
@@ -37,20 +37,26 @@ impl GyroSensor {
     /// Calibration ???
     pub const MODE_GYRO_CAL: &'static str = "GYRO-CAL";
 
+    /// Rotational Speed (2nd axis)
+    pub const MODE_TILT_RATE: &'static str = "TILT-RATE";
+
+    /// Angle (2nd axis)
+    pub const MODE_TILT_ANG: &'static str = "TILT-ANG";
+
     sensor!();
 
     /// Angle
-    pub fn set_mode_col_ang(&self) -> Ev3Result<()> {
+    pub fn set_mode_gyro_ang(&self) -> Ev3Result<()> {
         self.set_mode(Self::MODE_GYRO_ANG)
     }
 
     /// Rotational Speed
-    pub fn set_mode_col_rate(&self) -> Ev3Result<()> {
+    pub fn set_mode_gyro_rate(&self) -> Ev3Result<()> {
         self.set_mode(Self::MODE_GYRO_RATE)
     }
 
     /// Raw sensor value ???
-    pub fn set_mode_col_fas(&self) -> Ev3Result<()> {
+    pub fn set_mode_gyro_fas(&self) -> Ev3Result<()> {
         self.set_mode(Self::MODE_GYRO_FAS)
     }
 
@@ -64,31 +70,40 @@ impl GyroSensor {
         self.set_mode(Self::MODE_GYRO_CAL)
     }
 
+    /// Calibration ???
+    pub fn set_mode_tilt_rate(&self) -> Ev3Result<()> {
+        self.set_mode(Self::MODE_TILT_RATE)
+    }
+
+    /// Calibration ???
+    pub fn set_mode_tilt_ang(&self) -> Ev3Result<()> {
+        self.set_mode(Self::MODE_TILT_ANG)
+    }
+
     /// Gets the angle, ranging from -32768 to 32767
     /// Fails if it has been set in the wrong mode
     pub fn get_angle(&self) -> Ev3Result<i32> {
-        if self.get_mode()? == GyroSensor::MODE_GYRO_G_AND_A
-            || self.get_mode()? == GyroSensor::MODE_GYRO_ANG
-        {
-            return self.get_value0();
+        match self.get_mode()?.as_ref() {
+            GyroSensor::MODE_GYRO_G_AND_A => self.get_value0(),
+            GyroSensor::MODE_GYRO_ANG => self.get_value0(),
+            mode => Ev3Result::Err(Ev3Error::InternalError {
+                msg: format!("Cannot get angle while in {} mode", mode),
+                // Returns error
+            }),
         }
-        Ev3Result::Err(Ev3Error::InternalError {
-            msg: format!("Cannot get angle while in {}", self.get_mode()?),
-        })
     }
 
     /// Gets the rotational speed value, ranging from -440 to 440
     /// Fails is it has been set in the wrong mode:
     /// for example, fails if we ask for rotational speed while in MODE_GYRO_ANG mode
     pub fn get_rotational_speed(&self) -> Ev3Result<i32> {
-        if self.get_mode()? == GyroSensor::MODE_GYRO_RATE {
-            return self.get_value0();
-        } else if self.get_mode()? == GyroSensor::MODE_GYRO_G_AND_A {
-            return self.get_value1();
+        match self.get_mode()?.as_ref() {
+            GyroSensor::MODE_GYRO_RATE => self.get_value0(),
+            GyroSensor::MODE_GYRO_G_AND_A => self.get_value1(),
+            mode => Ev3Result::Err(Ev3Error::InternalError {
+                msg: format!("Cannot get rotational speed while in {} mode", mode),
+                // Returns error
+            }),
         }
-
-        Ev3Result::Err(Ev3Error::InternalError {
-            msg: format!("Cannot get rotational speed while in {}", self.get_mode()?),
-        })
     }
 }
